818e9e06494a106acb48890401abec1626df29ff
xhaeffer <subhansyahsp@gmail.com>
2024-12-09 19:12:43 +0700

    add logout placeholder in ProfileFragment

--- app/src/main/java/com/literify/ui/fragment/profile/ProfileFragment.kt
+++ app/src/main/java/com/literify/ui/fragment/profile/ProfileFragment.kt
@@ -1,17 +1,33 @@
 package com.literify.ui.fragment.profile
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.credentials.ClearCredentialStateRequest
+import androidx.credentials.CredentialManager
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
+import com.google.firebase.auth.FirebaseAuth
 import com.literify.databinding.FragmentProfileBinding
+import com.literify.ui.activity.auth.AuthActivity
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class ProfileFragment : Fragment() {
 
     private var _binding: FragmentProfileBinding? = null
     private val binding get() = _binding!!
+
+    @Inject
+    lateinit var firebaseAuth: FirebaseAuth
+
+    @Inject
+    lateinit var credentialManager: CredentialManager
 
     private val viewModel: ProfileViewModel by viewModels()
 
@@ -26,5 +42,20 @@ class ProfileFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        binding.btnLogout.setOnClickListener {
+            lifecycleScope.launch {
+                firebaseAuth.signOut()
+                credentialManager.clearCredentialState(ClearCredentialStateRequest())
+            }
+
+            if (firebaseAuth.currentUser == null) {
+                val intent = Intent(requireActivity(), AuthActivity::class.java).apply {
+                    flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                }
+                startActivity(intent)
+                requireActivity()
+            }
+        }
     }
 }
